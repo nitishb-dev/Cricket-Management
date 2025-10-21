532c3cf0a6714b33e7d8373189a69192393e14bf
NITISH B <nitishb057@gmail.com>
2025-10-21 08:48:19 +0530

    prod fix 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+const router = createBrowserRouter([{ path: '*', Component: App }]);
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <StrictMode>{<RouterProvider router={router} />}</StrictMode>
 );
